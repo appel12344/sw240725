aba013d834e8ea842460db3d8f5206b400cf5230
appel12344 <appel.3437@gmail.com>
2024-07-31 16:22:00 +0900

    Day05

---
+++ server/app copy.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    res.write('Hellowddddddddddddddddddddddddddddddddddd World!'); //write a response to the client
+    res.end(); //end the response
+});
+
+server.listen(8000, function() {
+    console.log("서버 실행 중 >>> http://localhost:8000");
+});
+
+// 실행 -> 터미널에 node ./app.js
